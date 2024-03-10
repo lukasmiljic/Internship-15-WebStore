02c5dac962d0ff191fd01f8d69db59084d3a2c95
lukasmiljic <smiljicluka13@gmail.com>
2024-03-10 15:23:16 +0100

    fixed product path

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import Home from "./Pages/Home/Home.jsx";
-import Products from "./Components/ProductList/ProductList.jsx";
+import Products from "./Pages/Products/Products.jsx";
 import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Products />} />
         <Route path="/search" element={<Products />} />
         <Route path="/search/:search" element={<Products />} />
       </Routes>
