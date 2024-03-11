186a30f2a30011279af4f500d7307dc155f0d600
lukasmiljic <smiljicluka13@gmail.com>
2024-03-11 19:14:57 +0100

    added header component

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,17 @@
+import SearchBar from "../../Components/SearchBar/SearchBar";
+import Logo from "../../Components/Logo/Logo";
+import classes from "./Header.module.css";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <header>
+      <Link className={classes.logo} to="/">
+        <Logo />
+      </Link>
+      <SearchBar />
+    </header>
+  );
+};
+
+export default Header;
