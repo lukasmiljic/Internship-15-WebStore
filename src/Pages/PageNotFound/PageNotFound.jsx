b51b3b28991ad2bd244918f8f0b7e87b84dd2eba
lukasmiljic <smiljicluka13@gmail.com>
2024-03-11 20:41:36 +0100

    added styling to 404 page

--- src/Pages/PageNotFound/PageNotFound.jsx
+++ src/Pages/PageNotFound/PageNotFound.jsx
@@ -1,7 +1,19 @@
+const divStyle = {
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  flexDirection: "column",
+};
+
+const h2Style = {
+  fontSize: "150px",
+  color: "#ffd700",
+};
+
 const pageNotFound = () => {
   return (
-    <div>
-      <h2>404</h2>
+    <div style={divStyle}>
+      <h1 style={h2Style}>404</h1>
       <p>Page not found</p>
     </div>
   );
