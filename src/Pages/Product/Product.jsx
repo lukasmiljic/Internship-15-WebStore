ddff41e4f82d86ced1df9c792ba67ef0d93f837e
lukasmiljic <smiljicluka13@gmail.com>
2024-03-11 20:34:52 +0100

    added styling to product page

--- src/Pages/Product/Product.jsx
+++ src/Pages/Product/Product.jsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import fetchProducts from "../../Data/FetchProducts";
+import Header from "../../Components/Header/Header";
+import classes from "./Product.module.css";
 
 const Product = () => {
   let [product, setProduct] = useState(null);
@@ -21,12 +23,33 @@ const Product = () => {
 
   return (
     <>
+      <Header />
       {product && (
-        <div>
-          <h1>{product.title}</h1>
-          <img src={product.image} alt={product.title} />
-          <p>{product.description}</p>
-          <p>{product.price}</p>
+        <div className={classes.productCard}>
+          <aside>
+            <img
+              className={classes.productImage}
+              src={product.image}
+              alt={product.title}
+            />
+          </aside>
+          <main>
+            <p className={classes.productTitle}>{product.title}</p>
+            <p className={classes.productDescription}>
+              {" "}
+              {product.description}{" "}
+            </p>
+            <p className={classes.productPrice}>
+              Price:{" "}
+              <span className={classes.price}>{product.price} &euro;</span>
+            </p>
+            <button className={classes.btn}>
+              <div className={classes.btnContent}>
+                Buy Now{" "}
+                <i class={`fa-solid fa-cart-shopping ${classes.btnIcon}`}></i>
+              </div>
+            </button>
+          </main>
         </div>
       )}
     </>
