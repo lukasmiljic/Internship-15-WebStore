feba745ca051e21b9f791d9c3380e7fac8863f5d
lukasmiljic <smiljicluka13@gmail.com>
2024-03-11 19:52:54 +0100

    tweaked header

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -5,12 +5,15 @@ import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <header>
-      <Link className={classes.logo} to="/">
-        <Logo />
-      </Link>
-      <SearchBar />
-    </header>
+    <>
+      <header>
+        <Link className={classes.logo} to="/">
+          <Logo />
+        </Link>
+        <SearchBar />
+      </header>
+      <hr />
+    </>
   );
 };
 
